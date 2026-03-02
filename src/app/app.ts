08152fae6b48c2158033afe1cdd7ad5a6801a30f
slippy404 <mikeedey2403@gmail.com>
2026-03-02 15:23:29 +0000

    app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListComponent } from './list-component/list-component';
+// Import Angular tools and the ListComponent used in this app
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ListComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css'#
+  // Defines the main App component and connects its template and styles
+  
 })
 export class App {
   count:number = 0;
   hide:boolean = false;
+  // Stores the counter value and a boolean to control visibility
+  
   onButtonClick(){
     this.count++;
   }
+  // Increases the counter when the button is clicked
+  
   onDoubleClick(){
     if (this.hide === false) {
       this.hide = true;
@@ -21,4 +28,5 @@ export class App {
       this.hide = false;
     }
   }
+  // Toggles the visibility when double-clicked
 }
